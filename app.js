544855d6414d954a7af96be44af22fa4619756be
Jalal <jalalk97@hotmail.com>
2023-03-15 19:53:22 -0400

    Exercise 11.9: Simple end to end -tests

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'))
+app.use(express.static(path.join(__dirname, 'dist')))
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
