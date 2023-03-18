3402b6d603a94336370b4239673e04f72ce33e94
Jalal <jalalk97@hotmail.com>
2023-03-18 17:35:03 -0400

    Testing failing deployment

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname, 'dist')))
 
+app.get('/health', (_req, res) => {
+  throw 'Error'
+  // eslint-disable-next-line no-unreachable
+  res.send('ok')
+})
+
+app.get('/version', (_req, res) => {
+  res.send('1')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5000')
